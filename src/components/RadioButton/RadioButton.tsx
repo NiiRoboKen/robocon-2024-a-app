c591f4d4df9ebb3eb9e768311cd6d5a870acbb46
Netetra <me@netetra.dev>
2024-08-22 14:01:59 +0900

    change: move to components/RadioButton/

---
+++ src/components/RadioButton/RadioButton.tsx
@@ -0,0 +1,26 @@
+type Props = {
+    options: string[];
+    selectedOption: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const RadioButton = (props: Props) => {
+  const { options, selectedOption, onChange } = props;
+
+  return (
+    <div>
+      {options.map((option) => (
+        <label key={option}>
+          <input
+            type="radio"
+            value={option}
+            name="select"
+            checked={selectedOption === option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </div>
+  );
+};

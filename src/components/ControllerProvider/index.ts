9c2f37f93203c0efd66bb2dfa4189f597ad8e13e
Netetra <me@netetra.dev>
2024-09-03 13:56:50 +0900

    fix: add index.ts

---
+++ src/components/ControllerProvider/index.ts
@@ -0,0 +1,1 @@
+export { ControllerContext, ControllerProvider } from "./ControllerProvider";
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ControllerProvider } from "./components/ControllerProvider/ControllerProvider.tsx";
+import { ControllerProvider } from "./components/ControllerProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

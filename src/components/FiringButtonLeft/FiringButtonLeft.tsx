2668a98761674c5ab3b7f8304eeb764adb35e4c8
3fuuuu <fuuuu1009@icloud.com>
2024-09-13 11:58:11 +0900

    refactir firnat

--- src/components/FiringButtonLeft/FiringButtonLeft.tsx
+++ src/components/FiringButtonLeft/FiringButtonLeft.tsx
@@ -1,13 +1,11 @@
 import { Button } from "../Button";
 
 export const FiringButtonLeft = () => {
-    return (
-        <div>
-            <Button
-            onClick= {() => {}}
-            >
-                <p>左発射</p>
-            </Button>
-        </div>
-    )   
-}
+  return (
+    <div>
+      <Button onClick={() => {}}>
+        <p>左発射</p>
+      </Button>
+    </div>
+  );
+};

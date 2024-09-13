0595bd945dd00f5a941368c125ad8ba55d86b526
3fuuuu <fuuuu1009@icloud.com>
2024-09-13 11:47:20 +0900

    feat: firing button left

---
+++ src/components/FiringButtonLeft/FiringButtonLeft.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../Button";
+
+export const FiringButtonLeft = () => {
+    return (
+        <div>
+            <Button
+            onClick= {() => {}}
+            >
+                <p>左発射</p>
+            </Button>
+        </div>
+    )   
+}

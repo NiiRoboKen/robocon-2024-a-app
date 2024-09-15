c89f2b9ef05ef5b534a7f0cb9bc9167d80788ab4
Netetra <me@netetra.dev>
2024-09-15 18:04:51 +0900

    create: collect button

---
+++ src/components/CollectButton/CollectButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../Button";
+
+export const CollectButton = () => {
+  return (
+    <div>
+      <Button onClick={() => {}}>
+        <p>回収</p>
+      </Button>
+    </div>
+  );
+};

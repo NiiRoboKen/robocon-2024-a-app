6eeba10f96be29aaee53e91d3214fb8a99296630
Netetra <me@netetra.dev>
2024-09-25 17:23:21 +0900

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { LeftFiringButton } from "./components/LeftFiringButton";
 import { RightFiringButton } from "./components/RightFiringButton";
 import { LeftTurnButton } from "./components/LeftTurnButton";
 import { RightTurnButton } from "./components/RightTurnButton";
-import classes from "./App.module.css";
+import style from "./App.module.css";
+
+const URL = "http://127.0.0.1:3000";
+const INTERVAL_TIME = 1000;
 
 const App = () => {
   const context = useContext(ControllerContext);
@@ -16,12 +19,11 @@ const App = () => {
   }
   const { setController } = context;
 
-  const URL = "http://127.0.0.1:3000";
-
   useEffect(() => {
     const interval = setInterval(() => {
       setController((prev) => {
         const body = JSON.stringify(prev);
+
         console.log(body);
         const res = fetch(URL, {
           method: "POST",
@@ -41,38 +43,38 @@ const App = () => {
 
         return prev.reset();
       });
-    }, 1000);
+    }, INTERVAL_TIME);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <>
-      <div className={classes.app}>
-        <div className={classes.left}>
-          <div className={classes.crosskey}>
+      <div className={style.app}>
+        <div className={style.left}>
+          <div className={style.crosskey}>
             <CrossKey />
           </div>
-          <div className={classes.joystick}>
+          <div className={style.joystick}>
             <JoyStick />
           </div>
         </div>
-        <div className={classes.button}>
-          <div className={classes.collect}>
+        <div className={style.button}>
+          <div className={style.collect}>
             <CollectButton />
           </div>
-          <div className={classes.firing}>
-            <div className={classes.leftfiring}>
+          <div className={style.firing}>
+            <div className={style.leftfiring}>
               <LeftFiringButton />
             </div>
-            <div className={classes.rightfiring}>
+            <div className={style.rightfiring}>
               <RightFiringButton />
             </div>
           </div>
-          <div className={classes.turn}>
-            <div className={classes.leftturn}>
+          <div className={style.turn}>
+            <div className={style.leftturn}>
               <LeftTurnButton />
             </div>
-            <div className={classes.rightturn}>
+            <div className={style.rightturn}>
               <RightTurnButton />
             </div>
           </div>

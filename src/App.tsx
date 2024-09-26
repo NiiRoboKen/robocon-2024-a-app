03c27ed497027f414ec63dd99a213e148994bd2c
yuyu2y <yuyu2yCo@gmail.com>
2024-09-26 17:29:42 +0900

    reset App.tsx and App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import { useContext, useEffect } from "react";
 import { ControllerContext } from "./components/ControllerProvider";
-import { CrossKey } from "./components/CrossKey";
-import { JoyStick } from "./components/JoyStick";
-import { CollectButton } from "./components/CollectButton";
-import { LeftFiringButton } from "./components/LeftFiringButton";
-import { RightFiringButton } from "./components/RightFiringButton";
-import { LeftTurnButton } from "./components/LeftTurnButton";
-import { RightTurnButton } from "./components/RightTurnButton";
 import classes from "./App.module.css";
 
 const App = () => {
@@ -18,7 +11,7 @@ const App = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setController(prev => {
+      setController((prev) => {
         console.log(prev);
         return prev.reset();
       });
@@ -28,37 +21,11 @@ const App = () => {
 
   return (
     <>
-      <div className={classes.app}>
-        <div className={classes.left}>
-          <div className={classes.crosskey}>
-            <CrossKey />
-          </div>
-          <div className={classes.joystick}>
-            <JoyStick />
-          </div>
-        </div>
-        <div className={classes.button}>
-          <div className={classes.collect}>
-            <CollectButton />
-          </div>
-          <div className={classes.firing}>
-            <div className={classes.leftfiring}>
-              <LeftFiringButton />
-            </div>
-            <div className={classes.rightfiring}>
-              <RightFiringButton />
-            </div>
-          </div>
-          <div className={classes.turn}>
-            <div className={classes.leftturn}>
-              <LeftTurnButton />
-            </div>
-            <div className={classes.rightturn}>
-              <RightTurnButton />
-            </div>
-          </div>
-        </div>
+      <div className={classes.left}>
+        <div></div>
+        <div></div>
       </div>
+      <div className={classes.right}></div>
     </>
   );
 };
